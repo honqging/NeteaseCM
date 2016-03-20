cee7abba4152ab6bd04c0a2b69707c81256ddb61
Qing Hong <hongqing131420@163.com>
2016-03-21 00:50:34 +0800

    just_for_contri

---
+++ just_for_contri/h5Test/serverSentEvent.php
@@ -0,0 +1,10 @@
+<!--serverSentEvent.php -->
+<?php
+	header('Content-Type:text/event-stream');
+	header('Cache-Control:no-cache');
+
+	$time = data('r');
+	echo "data: The server time is:{$time}\n\n";
+	flush();
+
+?>
